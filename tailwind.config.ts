edfff904d131237571699003140a979eae421ffc
Gabrielc1925 <gabrielc1925@gmail.com>
2024-11-29 06:47:30 -0700

    initial commit.  sveltekit up and running.

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import containerQueries from '@tailwindcss/container-queries';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
+import type { Config } from 'tailwindcss';
+
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [daisyui, typography, forms, containerQueries],
+
+	daisyui: {
+		themes: ['retro', 'luxury']
+	}
+} satisfies Config;
